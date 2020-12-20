f9060b272dd7532903887efec8fb857223a91430
diamondHoe420 <stachulski122@gmail.com>
2020-12-20 11:42:05 +0100

    pobieranie danych dotyczacych pogody ze strony

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -10,6 +10,19 @@ public class Program {
 
         try {
             Document doc = Jsoup.connect("https://pogoda.interia.pl/prognoza-dlugoterminowa-tabela,cId,1202046").get();
+
+
+            Elements rows = doc.getElementsByClass("weather-forecast-longterm-list-entry");
+
+            for (Element row : rows) {
+                Elements spans = row.getElementsByClass("weather-forecast-longterm-list-entry-hour").first().getElementsByTag("span");
+                String date = spans.get(0).text() + " " + spans.get(1).text();
+                String temp = row.getElementsByClass("weather-forecast-longterm-list-entry-forecast-temp").first().text();
+                String lowTemp = row.getElementsByClass("weather-forecast-longterm-list-entry-forecast-lowtemp").first().text();
+                String sun = row.getElementsByClass("weather-forecast-longterm-list-entry-forecast-phrase").first().getAllElements().first().text();
+
+                System.out.println(date + ", " + temp + " : " + lowTemp + ", " + sun);
+            }
         } catch (IOException e) {
             e.printStackTrace();
             return;
